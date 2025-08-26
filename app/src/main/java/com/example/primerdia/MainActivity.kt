34a77e9bb84b46ec491112ffa288ea0de3c82a10
apalo <apalomo809@gmail.com>
2025-08-26 13:36:12 -0600

    se agrego comentario en el main

--- app/src/main/java/com/example/primerdia/MainActivity.kt
+++ app/src/main/java/com/example/primerdia/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrimerDiaTheme {
+                //JOSE ANGEL PALOMO PEREZ
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
